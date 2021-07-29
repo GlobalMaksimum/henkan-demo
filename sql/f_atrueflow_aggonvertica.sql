6a86f383bb1d59a9d10f100d95821a0117769ddb
admin <admin@admin.com>
2021-07-29 12:15:40 +0000

    Convert to ELT

--- sql/f_atrueflow_aggonvertica.sql
+++ sql/f_atrueflow_aggonvertica.sql
@@ -1,4 +1,4 @@
-insert overwrite table lineitem_summ
+insert into lineitem_summ
 select l_orderkey, sum(L_QUANTITY) total_quantity,sum(L_EXTENDEDPRICE) total_price
 from lineitem
 group by l_orderkey;
