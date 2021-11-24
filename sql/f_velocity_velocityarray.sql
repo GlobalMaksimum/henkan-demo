6f5f8f05225020a6d72d4c043d17f5de2d200b05
gbm <gbm@gbm.com>
2021-11-24 09:17:53 +0000

    try has next

--- sql/f_velocity_velocityarray.sql
+++ sql/f_velocity_velocityarray.sql
@@ -1,5 +1,6 @@
-#set($foo = ["a", "b", "c"])
-#{foreach}($i in [0.. 2])
-insert into henkan_demo.velocity_array (name,id) values (foo.get($i),$i);
+#set($foo = [1, 2 , 3 ])
+#{foreach}($i in $foo)
+insert into henkan_demo.velocity_array (name,id) values (foo.get($i)::varchar,$i);
 commit;
+#if( $foreach.hasNext ),#end
 #{end}
