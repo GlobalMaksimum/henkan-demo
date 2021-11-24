7bad46299f12ead0a5672d0f4e5e8b1cbdc9a225
gbm <gbm@gbm.com>
2021-11-24 11:24:21 +0000

    parameter

--- sql/f_velocity_velocityloopbyvalue.sql
+++ sql/f_velocity_velocityloopbyvalue.sql
@@ -1,5 +1,5 @@
 #set($foo = $DynamicFilter.split(","))
 #{foreach}($i in $foo)
-update henkan_demo.filter_list set update_date=sysdate where name='$i';
+update henkan_demo.filter_list set update_date=sysdate where name='$i.trim()';
 commit;
 #{end}
