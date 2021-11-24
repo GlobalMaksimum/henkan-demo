5b8f3dc2c0f07a0159747ed09554b5190120167f
gbm <gbm@gbm.com>
2021-11-24 08:58:37 +0000

    velocity array

--- sql/f_velocity_velocityarray.sql
+++ sql/f_velocity_velocityarray.sql
@@ -1,5 +1,5 @@
-$sun.setPlanets('Earth', 'Mars', 'Neptune')
+#set($foo = ["a", "b", "c"])
 #{foreach}($i in [0.. 2])
-insert into henkan_demo.velocity_array (name,id) values (sun.getPlanets($i)),$i);
+insert into henkan_demo.velocity_array (name,id) values (foo.get($i),$i);
 commit;
 #{end}
