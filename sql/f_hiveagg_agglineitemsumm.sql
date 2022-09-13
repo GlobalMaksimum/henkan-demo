249d00451eeb1d78c434c8f93a149d5c34ea85f8
gbm <gbm@gbm.com>
2022-09-13 18:52:50 +0000

    changed schema

--- sql/f_hiveagg_agglineitemsumm.sql
+++ sql/f_hiveagg_agglineitemsumm.sql
@@ -1,4 +1,4 @@
-insert into default.lineitem_summ
+insert into henkan_demo.lineitem_summ
 select l_orderkey, sum(L_QUANTITY) total_quantity,sum(L_EXTENDEDPRICE) total_price
 from henkan_demo.lineitem
 group by l_orderkey
