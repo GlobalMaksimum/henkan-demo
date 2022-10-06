b9fab8cf9f91ebf6b0fb8b59fc66f81f4d8dd7cc
gbm <gbm@gbm.com>
2022-10-06 18:15:56 +0000

    fix

--- Flows/sql/f_kktcell_load_cdr_oracle_to_vertica.sql
+++ Flows/sql/f_kktcell_load_cdr_oracle_to_vertica.sql
@@ -1,3 +1,3 @@
 copy kktcell.cdr with source jdbcsource() parser jdbcloader( connect='oraclelab', 
-query='select * from kktcell.cdr where insert_date_ts>cast('${max_cdr_ts}' as timestamp)'
+query='select * from kktcell.cdr where insert_date_ts>cast(''${max_cdr_ts}'' as timestamp)'
 );
