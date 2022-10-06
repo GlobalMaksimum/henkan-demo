e006c2bbc026aab6e7ee1be53911664933c7f006
gbm <gbm@gbm.com>
2022-10-06 17:41:59 +0000

    fix

--- Flows/sql/f_kktcell_param_max_cdr_ts.sql
+++ Flows/sql/f_kktcell_param_max_cdr_ts.sql
@@ -1,1 +1,1 @@
-SELECT nvl(max(insert_date_ts),'1970-01-01 00:00:00') from kktcell.cdr_day0
+SELECT TO_CHAR(nvl(max(insert_date_ts),'1970-01-01 00:00:00'),'yyyy-mm-dd hh24:mi:ss.ff')  from kktcell.cdr_day0
