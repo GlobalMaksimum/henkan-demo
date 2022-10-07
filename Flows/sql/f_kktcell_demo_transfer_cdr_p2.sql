53c3cd74faa161c258461c56a5fe0d850cf5b385
gbm <gbm@gbm.com>
2022-10-07 10:38:36 +0000

    fix

--- Flows/sql/f_kktcell_demo_transfer_cdr_p2.sql
+++ Flows/sql/f_kktcell_demo_transfer_cdr_p2.sql
@@ -1,3 +1,3 @@
 copy kktcell.cdr with source jdbcsource() parser jdbcloader( connect='oraclelab', 
-query='select * from kktcell.cdr where insert_date_ts>to_timestamp(''${max_cdr_ts}'',''yyyy-mm-dd hh24:mi:ss.ff6'') and mod(call_id,4)=1'
+query='select * from kktcell.cdr where insert_date_ts>to_timestamp(''${max_cdr_ts}'',''yyyy-mm-dd hh24:mi:ss.ff6'') and mod(ora_hash(imsi),4)=1'
 );
