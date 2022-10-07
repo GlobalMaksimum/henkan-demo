ff7cdea2902eae910a73afef97b2bd2b7d5c9b74
gbm <gbm@gbm.com>
2022-10-07 09:21:57 +0000

    date

--- Flows/sql/f_kktcell_load_cdr_oracle_to_vertica.sql
+++ Flows/sql/f_kktcell_load_cdr_oracle_to_vertica.sql
@@ -1,3 +1,3 @@
 copy kktcell.cdr with source jdbcsource() parser jdbcloader( connect='oraclelab', 
-query='select * from kktcell.cdr where insert_date_ts>cast(''${max_cdr_ts}'' as timestamp)'
+query='select * from kktcell.cdr where insert_date_ts>to_timestamp(''${max_cdr_ts}'',''yyyy-mm-dd hh24:mi:ss.ff6'')'
 );
--- Flows/sql/f_kktcell_param_max_cdr_ts.sql
+++ Flows/sql/f_kktcell_param_max_cdr_ts.sql
@@ -1,1 +1,1 @@
- SELECT to_char(nvl(max(insert_date_ts),'1970-01-01 00:00:00')) from kktcell.cdr
+ SELECT to_char(nvl(max(insert_date_ts),'1970-01-01 00:00:00.000000')) from kktcell.cdr
