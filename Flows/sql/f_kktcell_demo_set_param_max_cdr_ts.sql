0e64bd1eda75d7b1d979b4dac90ba1a3c99b182f
gbm <gbm@gbm.com>
2022-10-07 10:27:08 +0000

    kktcell_demo

---
+++ Flows/sql/f_kktcell_demo_set_param_max_cdr_ts.sql
@@ -0,0 +1,1 @@
+ SELECT to_char(nvl(max(insert_date_ts),'1970-01-01 00:00:00.000000')) from kktcell.cdr
---
+++ Flows/sql/f_kktcell_demo_transfer_cdr_p2.sql
@@ -0,0 +1,3 @@
+copy kktcell.cdr with source jdbcsource() parser jdbcloader( connect='oraclelab', 
+query='select * from kktcell.cdr where insert_date_ts>to_timestamp(''${max_cdr_ts}'',''yyyy-mm-dd hh24:mi:ss.ff6'') and mod(imsi,4)=1'
+);
