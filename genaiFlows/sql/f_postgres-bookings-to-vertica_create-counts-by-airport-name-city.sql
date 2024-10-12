b4052e90df805c8e020f3693a6510c5cc3003c2a
inancdokurel <inanc.dokurel@globalmaksimum.com>
2024-10-12 16:14:03 +0300

    remove genai directory

--- genaiFlows/sql/f_postgres-bookings-to-vertica_create-counts-by-airport-name-city.sql
+++
@@ -1,8 +0,0 @@
-drop table bookings.counts_by_airport_name_city;
-
-create table bookings.counts_by_airport_name_city as select count(1) as count,
-                                                            ad.airport_name,
-                                                            ad.city,
-                                                            f.actual_arrival
-                                                     from bookings.flights f inner join bookings.airports_data ad on f.arrival_airport=ad.airport_code
-                                                     group by city,airport_name,actual_arrival order by count;
